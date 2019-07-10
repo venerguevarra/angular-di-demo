c3847fffa6bc09d5ac9af88b60cf03f5b5b9dd09
Vener Guevarra <vguevarra@citronsolutions.com>
2019-07-10 14:12:38 +0800

    Initial commit

---
+++ src/app/places/places.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { PlacesService } from '../service/places.service';
+import { Place } from '../model/place';
+
+@Component({
+  selector: 'app-places',
+  templateUrl: './places.component.html',
+  styleUrls: ['./places.component.css']
+})
+export class PlacesComponent {
+
+  title: string = "Places";
+  places: Place[];
+
+  constructor(private placesService: PlacesService) {
+    this.places = placesService.places;
+  }
+}
